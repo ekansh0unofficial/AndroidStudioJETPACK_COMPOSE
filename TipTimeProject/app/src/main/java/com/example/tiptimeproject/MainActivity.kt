9bd1b1da7c911998bd05f2ef884aadcbb48e9625
ekanshmittal <b422026@iiit-bh.ac.in>
2024-01-11 08:34:30 +0530

    git practise

---
+++ TipTimeProject/app/src/main/java/com/example/tiptimeproject/MainActivity.kt
@@ -0,0 +1,120 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.tiptimeproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tiptimeproject.ui.theme.TipTimeProjectTheme
+import java.text.NumberFormat
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipTimeProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TipTimeLayout()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TipTimeLayout() {
+
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.calculate_tip),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start)
+        )
+        EditNumberField()
+        Text(
+            text = stringResource(R.string.tip_amount, "$0.00"),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Spacer(modifier = Modifier.height(150.dp))
+    }
+}
+
+
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
+}
+
+@ExperimentalMaterial3Api
+@Composable
+fun EditNumberField(modifier :Modifier = Modifier){
+    //var amountInput = "0"
+  //  var amountInput : MutableState<String> = mutableStateOf("0")
+//    TextField(
+//        value = amountInput.value ,
+//        onValueChange ={amountInput.value = it} ,
+//        modifier = Modifier
+//            .padding(all = 10.dp)
+//            .background(color = Color.Green)
+//    )
+
+    var amountInput by remember {
+        mutableStateOf("")
+    }
+    TextField(
+        value = amountInput ,
+        onValueChange ={amountInput = it} ,
+        modifier = Modifier
+            .padding(all = 10.dp)
+            .background(color = Color.Green)
+    )
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+    )
+@Composable
+fun TipTimeLayoutPreview() {
+    TipTimeProjectTheme {
+        TipTimeLayout()
+    }
+}
