ab407a701e68364567c0ebfd5ea8c8cbf0c5bb45
ekanshmittal <b422026@iiit-bh.ac.in>
2024-03-29 23:32:10 +0530

    making a seprate repo for projects

--- DesertClicker/app/src/main/java/com/example/dessertclicker/MainActivity.kt
+++ DesertClicker/app/src/main/java/com/example/dessertclicker/MainActivity.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package com.example.dessertclicker
 
@@ -20,6 +5,7 @@ import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -71,10 +57,12 @@ import com.example.dessertclicker.data.Datasource
 import com.example.dessertclicker.model.Dessert
 import com.example.dessertclicker.ui.theme.DessertClickerTheme
 
+private const val TAG ="MainActivity"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        Log.d(TAG , "onCreate Called")
         setContent {
             DessertClickerTheme {
                 // A surface container using the 'background' color from the theme
@@ -88,7 +76,33 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG , "onPause Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG , "onDestroy Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG , "onStop Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG , "onResume Called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG , "onStart Called")
+    }
 }
+
 
 /**
  * Determine which dessert to show.
